9a8243ae528dcc5f4012aff9a67fc7bb300929cd
Chumak Slavik <chumakslav@gmail.com>
2023-02-19 18:13:11 +0200

    Added save/load from Local Storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,7 @@ import Filter from './Filter/Filter';
 import ContactForm from './ContactForm/ContactForm';
 
 const initialState = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
 };
 
@@ -17,6 +12,22 @@ class App extends Component {
   state = {
     ...initialState,
   };
+
+  componentDidMount() {
+    const contactsJSON = localStorage.getItem('contacts');
+
+    if (contactsJSON) {
+      const contacts = JSON.parse(contactsJSON);
+      this.setState(prevState => ({ contacts: contacts }));
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts != this.state.contacts) {
+      const contactsJSON = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', contactsJSON);
+    }
+  }
 
   handleChange = event => {
     const { name, value } = event.target;
